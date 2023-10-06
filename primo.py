e8466a8b7d627f7890c000add7db6e68f8884c07
Luis Fernando Galeano Martinez <144495208+GaleanoM@users.noreply.github.com>
2023-10-06 15:21:50 -0600

    Add files via upload

---
+++ primo.py
@@ -0,0 +1,23 @@
+def es_primo(numero):
+    if numero <= 1:
+        return False
+    elif numero <= 3:
+        return True
+    elif numero % 2 == 0 or numero % 3 == 0:
+        return False
+    i = 5
+    while i * i <= numero:
+        if numero % i == 0 or numero % (i + 2) == 0:
+            return False
+        i += 6
+    return True                             
+
+if __name__ == "__main__":
+    try:
+        numero = int(input("Ingrese un número entero: "))
+        if es_primo(numero):
+            print(f"{numero} es un número primo.")
+        else:
+            print(f"{numero} no es un número primo.")
+    except ValueError:
+        print("Entrada inválida. Por favor, ingrese un número entero válido.")
